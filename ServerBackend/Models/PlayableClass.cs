4895c3e3e88876a1be6cf3e69c791b1e405bcaeb
SiJiL82 <sven.lowry@gmail.com>
2021-05-26 16:21:30 +0100

    Added api.PlayableClass EF model

---
+++ ServerBackend/Models/PlayableClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ServerBackend.Models
+{
+    public partial class PlayableClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ServerBackend/Models/WoWGuildContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+#nullable disable
+
+namespace ServerBackend.Models
+{
+    public partial class WoWGuildContext : DbContext
+    {
+        
+                
+        public WoWGuildContext()
+        {
+        }
+
+        public WoWGuildContext(DbContextOptions<WoWGuildContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PlayableClass> PlayableClasses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = configuration["ConnectionStrings:WoWGuildWebsite"];
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<PlayableClass>(entity =>
+            {
+                entity.ToTable("PlayableClass", "api");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
