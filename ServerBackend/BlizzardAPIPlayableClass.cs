06c396b0ef88064fcc519dd2577f04deb070c260
SiJiL82 <sven.lowry@gmail.com>
2021-05-26 16:48:14 +0100

    Added read from and write to database

--- ServerBackend/BlizzardAPIPlayableClass.cs
+++ ServerBackend/BlizzardAPIPlayableClass.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using RestSharp;
 using ServerBackend.Models;
@@ -11,26 +12,61 @@ namespace ServerBackend
         //https://us.api.blizzard.com/data/wow/playable-class/7?namespace=static-us&locale=en_US&access_token=US28YW3liozF9sCTWUauEgdax5OktBzVtJ
 
         //Stores response from the API request
-        private IRestResponse apiResponse {get; set;}
-        public List<PlayableClass> apiResponseList {get; set;}
+        public List<PlayableClass> apiData {get; set;}
+        public List<PlayableClass> databaseData {get; set;}
 
 
         //Constructor, pass in region and authentication token
         public BlizzardAPIPlayableClass (string region, string token)
         {
-            apiResponseList = GetAPIData(region, token);
+            apiData = GetDataFromAPI(region, token);
+            databaseData = GetDataFromDatabase();
+            WriteToDatabase(apiData);
         }
 
-        private List<PlayableClass> GetAPIData(string region, string token)
+        //Pull latest data from the API
+        private List<PlayableClass> GetDataFromAPI(string region, string token)
         {
             string uri = "https://"+region+".api.blizzard.com/data/wow/playable-class/index?namespace=static-"+region+"&locale=en_US&access_token=" +token;
-            apiResponse = MakeAPIRequest(uri);
+            IRestResponse apiResponse = MakeAPIRequest(uri);
 
-            //DEBUG
+            //TODO: Add error handling here
+
+            /*//DEBUG
             Console.WriteLine(apiResponse.Content);
             //*/
             dynamic apiResponseJson = JsonConvert.DeserializeObject(apiResponse.Content);
             return apiResponseJson.classes.ToObject<List<PlayableClass>>();
+        }
+
+        //Pull latest data from the database
+        private List<PlayableClass> GetDataFromDatabase()
+        {
+            List<PlayableClass> results = new List<PlayableClass>();
+
+            using(WoWGuildContext database = new WoWGuildContext())
+            {
+                results = database.PlayableClasses.ToList();
+            }
+
+            /*//DEBUG
+            Console.WriteLine(results);
+            //*/
+
+            return results;
+        }
+
+        //Write API data to database
+        private void WriteToDatabase(List<PlayableClass> classes)
+        {
+            using(var db = new WoWGuildContext())
+            {
+                foreach(PlayableClass newClass in classes)
+                {
+                    db.Add(new PlayableClass {Name = newClass.Name, Id = newClass.Id});
+                }
+                db.SaveChanges();
+            }
         }
     }
 }
