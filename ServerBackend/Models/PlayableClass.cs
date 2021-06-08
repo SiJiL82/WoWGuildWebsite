7b7a4cac3bbc016919fbf12bf6afaf978e9f046d
SiJiL82 <sven.lowry@gmail.com>
2021-06-08 20:44:08 +0100

    Added equality check for list compare

--- ServerBackend/Models/PlayableClass.cs
+++ ServerBackend/Models/PlayableClass.cs
@@ -9,5 +9,21 @@ namespace ServerBackend.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (!(obj is PlayableClass))
+                throw new ArgumentException("obj is not an PlayableClass");
+            var usr = obj as PlayableClass;
+            if (usr == null)
+                return false;
+            return this.Id.Equals(usr.Id) && this.Name.Equals(usr.Name);
+        }
     }
+    
 }
